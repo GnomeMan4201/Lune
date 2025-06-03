acb39c4a4a3630a7309cbdfa6e2db9e5906fdbc5
ZeroCool <zerocool@cyberdeck.local>
2025-06-03 14:03:23 -0500

    Add Sphinx docs scaffold and ignore build artifacts

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project = 'LUNE'
+author = 'GnomeMan4201'
+release = '0.1'
+
+extensions = ['sphinx.ext.autodoc']
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
