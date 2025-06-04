59ee003c6a115a7776eed8b43ac798732fa6370a
ZeroCool <zerocool@cyberdeck.local>
2025-06-03 22:07:01 -0500

    Fix: basic docs structure to resolve CI doc deploy

--- docs/docs/docs/conf.py
+++ docs/docs/docs/conf.py
@@ -1,24 +1,21 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../lune'))
 
 project = 'Lune'
+copyright = '2025'
 author = 'Your Name'
-release = '0.1'
+release = '0.1.0'
 
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
     'sphinx.ext.autosummary',
-    'sphinx_rtd_theme',
 ]
-
-autosummary_generate = True
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-
